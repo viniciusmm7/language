d1bdce13138dab2f52191b42add7b04e0a01d7e6
viniciusmm7 <viniciusmm7@al.insper.edu.br>
2024-06-04 21:58:14 -0300

    Adding asm header and footer files

---
+++ asm/header.asm
@@ -0,0 +1,50 @@
+SYS_EXIT equ 1
+SYS_READ equ 3
+SYS_WRITE equ 4
+STDIN equ 0
+STDOUT equ 1
+True equ 1
+False equ 0
+
+segment .data
+
+formatin: db "%d", 0
+formatout: db "%d", 10, 0
+scanint: times 4 db 0
+
+segment .bss
+res RESB 1
+
+section .text
+global main
+extern scanf
+extern printf
+extern fflush
+extern stdout
+
+binop_je:
+JE binop_true
+JMP binop_false
+
+binop_jg:
+JG binop_true
+JMP binop_false
+
+binop_jl:
+JL binop_true
+JMP binop_false
+
+binop_false:
+MOV EAX, False  
+JMP binop_exit
+
+binop_true:
+MOV EAX, True
+
+binop_exit:
+RET
+
+main:
+
+PUSH EBP
+MOV EBP, ESP
